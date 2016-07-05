deac6c9cb508020004994adc286287fd0e9d93ef
Marcel Jacobs <silent.party@gmail.com>
2016-07-05 10:40:46 +1000

    setting up states

--- main.js
+++ main.js
@@ -4,15 +4,30 @@
  *
  * See the README.md for instructions
  */
-
-(function() {
-
+(function () {
   var container = document.querySelector('#container')
   var state = {}
 
   renderLoading(state, container)
 
-  function renderLoading(data, into) {
+  fetch('https://crossorigin.me/https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+Sydney&key=AIzaSyCT4WbbETNv8QhLddKci_onB8xcZC1hF1A')
+  .then((response) => {
+    return response.json()
+  })
+  .then((response) => {
+    console.log(response)
+  })
+  .catch((err) => {
+    console.log('error!', err)
+  })
+
+  function renderLoading (data, into) {
     // TODO: Add the template
+    // `<div id="pop-up" class="loader">${thing}</div>`
+  }
+
+  function renderList (state, into) {
+
   }
 })()
+
---
+++ notes.md
@@ -0,0 +1,33 @@
+# APIs
+
+Metacritic API (unofficial)
+https://www.publicapis.com/byroredux/metacritic
+
+SoundCloud
+https://developers.soundcloud.com/docs/api/guide
+
+GIPHY
+https://api.giphy.com/
+
+Google Places - CORS error
+https://developers.google.com/places/
+https://support.google.com/cloud/answer/6158862
+
+Mashape - lets you 'mash up' APIs
+https://market.mashape.com/ahmedakhan/video-game-information
+
+Spotify Web API (unofficial)
+https://market.mashape.com/jperez/spotify-web
+
+PublicAPIs
+https://www.publicapis.com/
+
+OMDb API
+http://www.omdbapi.com/
+
+Spotify Web API
+https://developer.spotify.com/web-api/
+
+Netflix roulette
+https://market.mashape.com/community/netflix-roulette
+
