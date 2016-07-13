2766ae6efaf98aacc24d3f1306bc3a25a356d581
Marcel Jacobs <silent.party@gmail.com>
2016-07-13 10:40:05 +1000

    cleaned up comments, added scaffolding for search

--- main.js
+++ main.js
@@ -7,13 +7,16 @@
 'use strict'
 
 ;(function () {
-  var container = document.querySelector('#container')
-  var popupContainer = document.querySelector('#popup-container')
+
+  const container = document.querySelector('#container')
+  const popupContainer = document.querySelector('#popup-container')
+  const header = document.querySelector('header')
+  const imgPlaceholder = './images/r_placeholder.png'
+  const defaultSubreddit = 'https://www.reddit.com/r/cats.json'
+
   var state = {}
-  var header = document.querySelector('header')
-  var redditTopic = 'cats'
-  var imgPlaceholder = './images/r_placeholder.png'
-  var defaultSubreddit = 'https://www.reddit.com/r/cats.json'
+
+  let redditTopic = 'cats'
 
 // NOTE:Fetch subreddit urls
   function fetchSubreddit (url) {
@@ -26,42 +29,45 @@
         .then((json) => {
           state.postData = json.data.children.map((item, index) => {
             let obj = {}
-            obj.postTitle = item.data.title
-            obj.postBody = item.data.selftext
-            obj.postSrcUrl = item.data.url
-            obj.score = item.data.score
-            obj.author = item.data.author
+            let data = item.data
+            obj.postTitle = data.title
+            obj.postBody = data.selftext
+            obj.postSrcUrl = data.url
+            obj.score = data.score
+            obj.author = data.author
             obj.postId = index
-            // TODO: Write a function for the below that looks for a URL in a string. The edge cases for the below could go on forever otherwise, so just look for what's needed.
+            // TODO: Write a function for the below that looks for a jpg / jpeg extension in a string. The edge cases for the below could go on forever otherwise, so just look for what's needed.
             obj.imageThumbUrl = ((item.data.thumbnail === '') || (item.data.thumbnail === 'default') || (item.data.thumbnail === 'self')) ? imgPlaceholder : item.data.thumbnail
             item.data.preview ? obj.imageUrl = item.data.preview.images[0].source.url : obj.imageUrl = imgPlaceholder
             return obj
           })
-          // console.log('state object')
-          // console.log(state)
           renderList(state.postData, container)
-          // console.log('from fetchSubreddit: ')
-          // console.log(state.postData)
         })
         .catch((err) => {
-          console.log('there\'s been a problem', err)
+          console.warn('there\'s been a problem', err)
         })
     }
   }
 
-  // function  getStuff(eventName) {
-  //   return stuff === eventName
-  // }
-
-  // function findObject (id, resolve) {
-  //   state.items.forEach((obj) => {
-  //     var key = Object.keys(obj).filter((key) => {
-  //       if (obj[key] === id) {
-  //         resolve(obj)
-  //       }
-  //     })[0]
-  //   })
-  // }
+  // Grab subreddit categories
+  var subredditsByTopicUrl = 'https://www.reddit.com/api/subreddits_by_topic.json?query=' + redditTopic
+  fetch(subredditsByTopicUrl)
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (json) {
+    state.feedData = []
+    state.feedData = json.map((item) => {
+      var obj = {}
+      obj.feedTitle = item.name
+      obj.feedUrl = 'https://www.reddit.com/r/' + item.name + '.json'
+      return obj
+    })
+    renderHeader(state, header)
+  })
+  .catch(function (err) {
+    console.log('something went wrong:' + err)
+  })
 
   delegate('body', 'click', 'ul.dropdown li a', function (event) {
     var thisFeedUrl = (val) => {
@@ -69,19 +75,19 @@
       state.feedData.forEach((item) => {
         if (item.feedTitle === event.delegateTarget.dataset.category) {
           val = item.feedUrl
-          state.feedData.currentFeed = item.feedTitle
+          state.feedData.selectedFeed = item.feedTitle
         }
       })
       return val
     }
     fetchSubreddit(thisFeedUrl())
-    renderHeader(state, header)
+    // TODO: update selectedFeed with current category. Then, re-render menu.
+    let menuHtml = renderMenu()
   })
 
   delegate('body', 'click', '.article-content a', function (event) {
     event.preventDefault()
-    // TODO: Try using history API to do this stuff ?
-    // i.e. history.pushState('foo', 'bar', '#zip')
+    // TODO: Try using history API to do this stuff?
     var thisId = parseInt(event.delegateTarget.dataset.id, 10)
     state.postData.forEach((obj) => {
       if (thisId === obj.postId) {
@@ -98,49 +104,44 @@
     })
   })
 
-// Grab subreddit categories
-  var subredditsByTopicUrl = 'https://www.reddit.com/api/subreddits_by_topic.json?query=' + redditTopic
-  fetch(subredditsByTopicUrl)
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (json) {
-      state.feedData = []
-      state.feedData = json.map((item) => {
-        var obj = {}
-        obj.feedTitle = item.name
-        obj.feedUrl = 'https://www.reddit.com/r/' + item.name + '.json'
-        return obj
-      })
-      renderHeader(state, header)
-    })
-    .catch(function (err) {
-      console.log('something went wrong:' + err)
-    })
-  // TODO: renderHeader needs to update currentFeed no matter how feed is loaded, not just on click of menu. (like when page inits, for example, there's no click action, so the header doesn't update to show currentFeed)
-  function renderHeader (state, into) {
-    into.innerHTML = `
-      <section class="wrapper">
-        <a href="#"><h1>Feedr</h1></a>
-        <nav>
-          <section id="search">
-            <input type="text" name="name" value="">
-            <div id="search-icon"><img src="images/search.png" alt="" /></div>
-          </section>
-          <ul>
-            <li><a href="#">News Source: <span>${state.feedData.currentFeed}</span></a>
-            <ul class="dropdown">
-              ${state.feedData.map((item) => {
-                return `<li>${renderItem(item.feedTitle)}</li>`
-              }).join('')}
-            </ul>
-            </ul>
-            </li>
+  // TODO: Search stuff
+  delegate('body', 'keyup', '#search-input', function (event) {
+    let val = event.delegateTarget.value
+    console.log(val)
+  })
+
+  function renderMenu (data) {
+    let selectedFeed = (!data.hasOwnProperty('selectedFeed')) ? 'All' : data.feedData.selectedFeed
+    console.log("selectedFeed", selectedFeed)
+    return `
+      <ul>
+        <li><a href="#">News Source: <span>${selectedFeed}</span></a>
+          <ul class="dropdown">
+            ${data.feedData.map((item) => {
+              return `<li>${renderItem(item.feedTitle)}</li>`
+            }).join('')}
           </ul>
-        </nav>
-        <div class="clearfix"></div>
-      </section>
+        </li>
+      </ul>
       `
+  }
+
+  function renderHeader (data, into) {
+    // console.log(state)
+    let menu = renderMenu(data)
+    into.innerHTML = `
+    <section class="wrapper">
+      <a href="#"><h1>Feedr</h1></a>
+        <nav>
+        <section id="search">
+        <input type="text" id="search-input" name="name" value="">
+        <div id="search-icon"><img src="images/search.png" alt="" /></div>
+        </section>
+        ${menu}
+        </nav>
+      <div class="clearfix"></div>
+    </section>
+    `
   }
 
   function renderItem (item) {
@@ -153,9 +154,14 @@
     into.innerHTML = `<div id="pop-up" class="loader"></div>`
   }
 
+  function filterState () {
+  // TODO: Create a new state object, containing only the results that match the search input
+  // This should run on each keyup event
+  }
+
+  // *** BEGIN VIEW ***
   function renderList (data, into) {
-    // console.log('from renderList ')
-    // console.log(data)
+    console.log(data)
     into.innerHTML = `
     <section id="main" class="wrapper">
         ${data.map((item) => {
@@ -195,9 +201,9 @@
     </div>
     `
   }
+
   // init
   (() => {
     fetchSubreddit(defaultSubreddit)
-    renderHeader(state, header)
   })()
 })()
